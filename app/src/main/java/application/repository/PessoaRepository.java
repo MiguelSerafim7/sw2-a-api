29ad2df41e961be09a00ed55226a83f59080d739
MiguelSerafim7 <miguel.serafim.ptal@gmail.com>
2024-09-13 12:23:39 +0000

    aula13/09

---
+++ app/src/main/java/application/repository/PessoaRepository.java
@@ -0,0 +1,12 @@
+package application.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import application.model.Pessoa;
+
+public interface PessoaRepository extends CrudRepository<Pessoa, Long>  {
+
+    
+ 
+    
+}
